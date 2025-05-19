1a625090ef531ecdfab13ac2c80e067b75b3dcb8
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-19 15:07:35 -0700

    Completes activity requirements.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // ======================
 //      VARIABLES
 // ======================
-// veggie = "carrot"
-// veggie = "onion"
+let veggie = 'carrot';
+veggie = 'onion';
 
 //
 //
@@ -10,11 +10,11 @@
 //      DATA STRUCTURES
 // =========================
 
-// animals = ["tiger", "panda", "raccoon"]
-// animals.append("opossum")
+const animals = ['tiger', 'panda', 'raccoon'];
+animals.push('opossum');
 
-// dog = {"name": "Sprinkles", "age": 3}
-// dog["is_friendly"] = True
+const dog = { name: 'Sprinkles', age: 3 };
+dog.isFriendly = true;
 
 //
 //
@@ -22,15 +22,18 @@
 //       LOOPS
 // =================
 
-// num = 5
-// for i in range(num):
-//    print(i)
+const num = 5;
+for (let i = 0; i < num; i++) {
+  console.log(i);
+}
 
-// for animal in animals:
-//    print(animal)
+for (const animal of animals) {
+  console.log(animal);
+}
 
-// for k, v in dog.items():
-//    print(f"{k}: {v}")
+for (const [key, value] of Object.entries(dog)) {
+  console.log(`${key}: ${value}`);
+}
 
 //
 //
@@ -38,20 +41,17 @@
 //       FUNCTIONS / CONDITIONALS
 // ===================================
 
-// def fizzbuzz(num):
-//    if num % 3 == 0 and num % 5 == 0:
-//        return "fizzbuzz"
-//    elif num % 3 == 0:
-//        return "fizz"
-//    elif num % 5 == 0:
-//        return "buzz"
-//    else:
-//        return num
-
-// Old way: Function Declaration
-
-//
-// ES6 way: Function Expression
+const fizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
+};
 
 //
 //
@@ -59,18 +59,23 @@
 //       CLASSES
 // ======================
 
-// class Animal:
-//    def __init__(self,species,name):
-//        self.species = species
-//        self.name = name
-//
-//    def __str__(self):
-//        return f"{self.name} is a {self.species}"
+class Animal {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
 
-// class Dog(Animal):
-//    def __init__(self, name, trick):
-//        super().__init__(species="dog", name=name)
-//        self.trick = trick
-//
-//    def __str__(self):
-//        return f"{super().__str__()} that can {self.trick}"
+  toString() {
+    return `${this.name} is a ${this.species}`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, trick) {
+    super('dog', name);
+    this.trick = trick;
+  }
+  toString() {
+    return `${super.toString()} that can ${this.trick}`;
+  }
+}
